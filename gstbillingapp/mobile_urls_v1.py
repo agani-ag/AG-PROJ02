0a4a227a40cc41a83f5e4a7befa584c54a9001f4
Ganesh <ganesh.s@ciglobalsolutions.com>
2026-01-09 21:35:45 +0530

    ENH-V7C04 → Admin Payment Active
    ENH-V7C04 → Admin Payment Active

--- gstbillingapp/mobile_urls_v1.py
+++ gstbillingapp/mobile_urls_v1.py
@@ -8,20 +8,20 @@ from .views.mobile_v1 import (
 
 urlpatterns = [
     # Customer URLs
-    path('customer/profile', customer.customer_profile, name='v1customerprofile'),
     path('customer/home', customer.customer_home, name='v1customerhome'),
     path('customer/books', customer.customer_books, name='v1customerbooks'),
+    path('customer/profile', customer.customer_profile, name='v1customerprofile'),
     path('customer/invoices', customer.customer_invoices, name='v1customerinvoices'),
     path('customer/invoice_viewer/<int:invoice_id>', customer.customer_invoice_viewer, name='v1customerinvoiceviewer'),
     
     # Employee URLs
-    path('customers', customer.customers, name='v1customers'),
-    path('invoices', customer.invoices, name='v1invoices'),
-    path('books', customer.books, name='v1books'),
-    path('expensestracker', customer.expenses_tracker, name='v1expensestracker'),
-    path('purchaselogs', customer.purchase_logs, name='v1purchaselogs'),
-    path('products', customer.products, name='v1products'),
     path('home', customer.home, name='v1home'),
+    path('books', customer.books, name='v1books'),
+    path('invoices', customer.invoices, name='v1invoices'),
+    path('products', customer.products, name='v1products'),
+    path('customers', customer.customers, name='v1customers'),
+    path('purchaselogs', customer.purchase_logs, name='v1purchaselogs'),
+    path('expensestracker', customer.expenses_tracker, name='v1expensestracker'),
 
     # API URLs
     path('customers/api', customer.customersapi, name='v1customersapi'),
